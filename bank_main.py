35063afa398fd7dc0f7ab7eb37212c34059a0905
GeorgijsVagalis <98511693+GeorgijsVagalis@users.noreply.github.com>
2022-01-27 08:50:10 +0200

    deletef property added new method

--- bank_main.py
+++ bank_main.py
@@ -20,12 +20,11 @@ class Account:
         Account.auto_account_number += 1
         self.currency = currency
         self.initial_balance = initial_balance
-        self.timestamp = datetime.datetime.now()
+        self.timestamp = datetime.datetime.now()                   
 
-    @property #decorators
-    def account_number(self):
-        return self._account_number
-
+    def print_transactions():
+        pass
+    
 class Client:
     def __init__(self, name: str):
         self.name = name
